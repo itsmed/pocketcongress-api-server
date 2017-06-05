0d3de835e5bee106a03423b7ee48fbff9d1c8150
DejonGill <dejonjgill@gmail.com>
2017-06-04 18:01:21 -0700

    experiment

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,28 @@ module.exports = function(app) {
 
   app.post('/api/reps/all/federal/by-district', federalReps.getFederalRepsByDistrict);
 
+
+
+
+
+  app.get('/', (req, res) => {
+    res.send(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <style>
+          body {
+            background-color: gray;
+            font-family: sans-serif;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+      </html>
+    `);
+  });
+
 };
