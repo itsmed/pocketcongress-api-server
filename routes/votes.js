bdb1f1b3bb56fa6ec72db727fec3fd98e137ad2c
DejonGill <dejonjgill@gmail.com>
2017-07-04 17:17:44 -0700

    database seeding methods

--- routes/votes.js
+++ routes/votes.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const { db } = require('../firebase-init');
 
-axios.defaults.headers['X-API-KEY'] = process.env.PROPUBLICA_API_KEY;
+axios.defaults.headers['X-API-KEY'] = 'PsVtJffI796o7nlUmj0ISm53HnkgtDl9Bay1urC2' //process.env.PROPUBLICA_API_KEY;
 
 exports.getVotesByDate = function(req, res) {
   const { month, year } = req.body.data;
@@ -15,8 +15,6 @@ exports.getVotesByDate = function(req, res) {
   .then(responses => {
     const allData = {};
 
-    console.log(responses[0].data.results);
-
     const filteredResponses = responses.map(resp => Object.assign({}, resp.data.results, {
       votes: resp.data.results.votes.filter(r => r.bill || r.nomination)
     }));
@@ -24,7 +22,10 @@ exports.getVotesByDate = function(req, res) {
 
     filteredResponses.forEach(resp => {
 
-      resp.votes.forEach(v => v.bill ? seedDatabase(v.congress, v.bill.number.toLowerCase().replace(/\W/g, '')) : '');
+      resp.votes.forEach(v => {
+        seedDatabaseWithVoteData(v.congress, resp.chamber.toLowerCase(), v.session, v.roll_call);
+        v.bill ? seedDatabaseWithBillData(v.congress, v.bill.number.toLowerCase().replace(/\W/g, '')) : '';
+      });
 
       allData[resp.chamber] = resp;
     });
@@ -74,8 +75,55 @@ exports.getSpecificBill = function(req, res) {
 
 };
 
-function seedDatabase(congress, billId) {
-  // console.log('seed database', congress, billId);
+function seedDatabaseWithVoteData(congress, chamber, session, rollCall) {
+  const ref = db.ref(`votes/${congress}/${chamber}/${session}/${rollCall}`);
+  axios.get(`https://api.propublica.org/congress/v1/${congress}/${chamber}/sessions/${session}/votes/${rollCall}.json`)
+    .then(res => {
+      if (res.data.status === 'ERROR') {
+        return console.log('FUCKED', res.data.errors);
+      }
+      res.data.results.votes.vote.positions
+        .forEach(p => {
+          ref.push({ id: p.member_id, position: p.vote_position }, err => {
+            if (err) {
+              console.log('fuk', err.message);
+            } else {
+              console.log('saved junk');
+            }
+          });
+        });
+    })
+    .catch(err => console.log('why erroring?', err.message));
+}
+
+
+function seedDatabaseWithRepData(id) {
+  const ref = db.ref(`federalReps/${id}`);
+
+  ref.once('value', function(snap) {
+    if (snap.val() === null) {
+      return axios.get(`https://api.propublica.org/congress/v1/members/${id}.json`)
+        .then(res => {
+          if (!res.data.results) {
+            return console.log('error', res.data.error);
+          }
+          ref.set(res.data.results[0], function(err) {
+            if (err) {
+              return Promise.reject(err);
+            }
+            console.log('success');
+          });
+        })
+        .catch(err => console.log("no luck", err.message));
+    } else {
+      return console.log('already in database', snap.val());
+    }
+
+  });
+}
+
+
+function seedDatabaseWithBillData(congress, billId) {
   const ref = db.ref(`bills/${congress}/${billId}`);
 
   ref.once('value', function(snap) {
@@ -109,3 +157,18 @@ function seedDatabase(congress, billId) {
     }
   });
 }
+
+// const { senateIds } = require('../Senate');
+// const { houseIds } = require('../House');
+// console.log(senateIds, houseIds);
+
+// const allIds = senateIds.concat(houseIds);
+
+// allIds.forEach(id => seedDatabaseWithRepData(id));
+
+db.ref('/federalReps')
+  // .orderByValue()
+  .once('value', function(snap) {
+    console.log('length', typeof snap.val(), Object.keys(snap.val()).length);
+  });
+
